7c8b0864ebd33d15076885e0f7987ac0627340d0
mah <zanzinzzo@gmail.com>
2023-04-26 19:45:24 -0300

    dup2

---
+++ ch1/dup2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			file, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(file, counts)
+			file.Close()
+		}
+	}
+	for line, number := range counts {
+		if number > 1 {
+			fmt.Printf("%d\t%s\n", number, line)
+		}
+	}
+}
+
+func countLines(file *os.File, counts map[string]int) {
+	input := bufio.NewScanner(file)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
