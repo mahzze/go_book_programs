4657d87c6392cb580d0192c2f8d8478eb2cbe764
mah <zanzinzzo@gmail.com>
2023-04-30 23:57:08 -0300

    server3

---
+++ ch1/server3.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+// handler echoes the HTTP request. This time it is used as a kind of tool to debug.
+func handler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "%s %s %s\n", request.Method, request.URL, request.Proto)
+	for key, value := range request.Header {
+		fmt.Fprintf(writer, "Header[%q] = %q\n", key, value)
+	}
+	fmt.Fprintf(writer, "Host = %q\n", request.Host)
+	fmt.Fprintf(writer, "Remote Address= %q\n", request.RemoteAddr)
+
+	if err := request.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for key, value := range request.Form {
+		fmt.Fprint(writer, "Form[%q] = %q\n", key, value)
+	}
+}
