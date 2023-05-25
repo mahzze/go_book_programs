1fe00ec5276e6923051fcf471ff8c752fd756db2
mah <zanzinzzo@gmail.com>
2023-05-25 19:15:53 -0300

    tempconv exercise 2.1

--- ch2/2.6/tempconv/tempconv.go
+++ ch2/2.6/tempconv/tempconv.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
@@ -11,5 +12,6 @@ const (
 	BoilingC      Celsius = 100
 )
 
+func (kelvin Kelvin) String() string     { return fmt.Sprintf("%gK", kelvin) }
 func (celsius Celsius) String() string   { return fmt.Sprintf("%g°C", celsius) }
 func (fahren Fahrenheit) String() string { return fmt.Sprintf("%g°F", fahren) }
